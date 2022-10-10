5cdfb3dab8445cfa73da897ad3cddfbd481c5d49
mohamed khan <mohamedkhan@mohameds-MacBook-Pro.local>
2022-10-10 17:04:08 -0400

    fixing bugs

--- index.cjs
+++ index.cjs
@@ -3,7 +3,7 @@ import path from "path";
 const __dirname = path.resolve();
 import fs from "fs";
 //reads contents of info.json
-fs.readFile(__dirname + "/info.json", "utf8", (err, data) => {
+fs.readFile(__dirname + "./info.json", "utf8", (err, data) => {
   if (err) {
     console.log(err);
   } else {
