763d15f3a9b26b860548fbe87a11d3405a497161
mohamed khan <mohamedkhan@mohameds-MacBook-Pro.local>
2022-10-10 17:09:14 -0400

    fixing bugs

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 #! /usr/bin/env node
-import path from "path";
-const __dirname = path.resolve();
-import fs from "fs";
-//reads contents of info.json
+
+const fs = require("fs");
+
 fs.readFile(__dirname + "./info.json", "utf8", (err, data) => {
   if (err) {
     console.log(err);
